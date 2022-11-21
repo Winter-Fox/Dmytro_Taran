ff2c5228ee7de8eb94d7bca1dec1b64f09b4fab2
Winter Fox <56193688+Winter-Fox@users.noreply.github.com>
2022-11-21 03:58:20 +0300

    Test commit
    Test commit to check how this thing works with solution files

---
+++ C#_Basic_Knowledge/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        public void Do()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#_Basic_Knowledge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.Do();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
