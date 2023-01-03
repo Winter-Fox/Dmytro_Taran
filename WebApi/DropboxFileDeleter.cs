72948ddf612ecaf580deea0d34e1e6b3fd396079
Winter Fox <56193688+Winter-Fox@users.noreply.github.com>
2023-01-03 22:10:21 +0300

    Add files via upload

---
+++ WebApi/DropboxFileDeleter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi
+{
+    public class DropboxFileDeleter : DropboxFactoryClass
+    {
+        public override bool CheckFileExist()
+        {
+            DropboxFileMetadata dfm = new DropboxFileMetadata();
+            return dfm.CheckFileExist();
+        }
+
+        public override void ShowSuccessMessage()
+        {
+            Console.WriteLine("You succefully deleted file!");
+        }
+
+        public void DeleteFile()
+        {
+            if (CheckFileExist())
+            {
+                // Loop through all files in dropbox and if found one with given name retrun it's metadata
+                var filesInDropbox = DropboxClient.Files.ListFolderAsync(string.Empty);
+                foreach (var item in filesInDropbox.Result.Entries)
+                {
+                    if (item.Name == fileName)
+                    {
+                        DropboxClient.Files.DeleteV2Async(item.PathLower);
+                    }
+                }
+            }
+        }
+    }
+}
