e96fb45b2ae0f3228d8a7f893400579ea23cb44f
Winter Fox <56193688+Winter-Fox@users.noreply.github.com>
2023-01-03 22:17:12 +0300

    Add files via upload

---
+++ WebApi/DropboxFactoryClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dropbox.Api;
+
+namespace WebApi
+{
+    public abstract class DropboxFactoryClass
+    {
+        // Fields universal for every class
+        static private string token = "sl.BWMljq3rQq4NjATptu9eounDJrMSAVbK-pFtUphvyd6OKTl_jGsVxPpzDYMBR0vluY1Vh2ebDptrXGkgx-CulhBMvfcU_3amvoW4ZzFrdX_ALa3h3eNiS_hwlnBWPrBrxh6GgReznzWy";
+        public DropboxClient DropboxClient = new DropboxClient(token);
+        public string fileName = @"FileName.txt";
+
+        // Mehtod which checks whether file exists (either on PC or on Dropbox)
+        public abstract bool CheckFileExist();
+        // Method which shows message of operation success
+        public abstract void ShowSuccessMessage();
+    }
+}
